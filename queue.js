9d1a6e7593412bd6ae35cbfffeea850f3db65a30
Michael <checomichael2@gmail.com>
2019-03-08 19:39:05 -0500

    Queue Data Structure

---
+++ queue.js
@@ -0,0 +1,31 @@
+//FIFO
+
+function createQueue() {
+	const queue = [];
+	return {
+		// add or enqueue
+		enqueue(item) {
+			queue.unshift(item);
+		},
+		// remove or dequeue
+		dequeue() {
+			return queue.pop();
+		},
+
+		// peek
+		peek() {
+			return queue[queue.length - 1];
+		},
+		// length
+		get length() {
+			return queue.length;
+		},
+		// isEmpty
+		isEmpty() {
+			return queue.length === 0;
+		},
+	};
+}
+
+const q = createQueue();
+console.log(q.isEmpty());
